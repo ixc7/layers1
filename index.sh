8647a2c1f17a86724d46ca786069b8a9c5590d9a
ixc7 <isaacxpreston7@gmail.com>
2022-01-24 09:38:47 -0500

    refactor, notes

--- index.sh
+++ index.sh
@@ -3,86 +3,55 @@
 source "./utils.sh"
 source "./spacing.sh"
 
-trap cleanup EXIT SIGINT
-
-###################
-#### constants ####
-###################
-
 promptTxt="press <ANY KEY> to generate another image"
 quitTxt="press <q> to quit"
 imgFile=$(mktemp)
 txtFile=$(mktemp)
 
+trap cleanup EXIT SIGINT
+
 while true; do
-
-  ###############
-  #### setup ####
-  ###############
-
   clearScreen
   maxHeight=$(setMaxHeight)
-  
+
+  # render random image + text  
   randomTxt "${txtFile}"
   randomImg "${imgFile}"
   defineMargins "img" "${imgFile}"
   defineMargins "txt" "${txtFile}"
-  
+  render "${imgFile}" "${imgYMargin}" "${imgXSpace}" "${maxHeight}"
+  render "${txtFile}" "${txtYMargin}" "${txtXSpace}" "${maxHeight}"
+
   # get positions for 'keypress' and 'quit' prompts
-  # use defineMargins?
   promptXMargin=$(getXMargin "${promptTxt}")
   promptXSpace=$(printXMargin "${promptXMargin}")
   quitXMargin=$(( $(( $(tput cols) - ${#quitTxt} )) / 2 ))
   quitXSpace=$(printXMargin ${quitXMargin})
-  
-  # replace 'keypress' prompt whitespace w/ esc chars
-  formattedPrompt=$(printSpace "${promptXSpace}${promptTxt}")
 
-
-  #############################
-  #### render image + text ####
-  #############################
-  
-  render "${imgFile}" "${imgYMargin}" "${imgXSpace}" "${maxHeight}"
-  render "${txtFile}" "${txtYMargin}" "${txtXSpace}" "${maxHeight}"
-
-
-  ########################
-  #### render prompts ####
-  ########################
-
-  # place 'quit' prompt 1 line below the image
-  [[ ${imgYMargin} -le 1 ]] && 
-    # if image takes up entire screen height
+  # position 'quit' text 2 lines below the image
+  # if image takes up entire screen height, position 1 line below image
+  [[ ${imgYMargin} -le 2 ]] && 
     quitPos=$(( imgHeight + 1 )) ||
-    # else, if image can be centered on screen
     quitPos=$(( imgHeight + imgYMargin + 1 ))
+    
+  # if position is below the screen, position at bottom
+  [[ ${quitPos} -ge $(( $(tput lines) - 2 )) ]] && 
+    qcursorPos=$(($(tput lines) - 1)) ||
+    qcursorPos=$((imgHeight + imgYMargin + 2))
 
-  (
-    # if quit position is below the screen, move up 1 line
-    [[ ${quitPos} -ge $(( $(tput lines) - 2 )) ]] &&
-    cursorTo $(($(tput lines) - 1)) 0
-  ) || 
-  # else, position normally
-  # cursorTo $((imgHeight + imgYMargin + 1)) 0
-  cursorTo $((imgHeight + imgYMargin + 2)) 0
-  echo -e "${quitXSpace}${quitTxt}"
+  # render 'quit' text
+  cursorTo ${qcursorPos} 0
+  echo -ne "${quitXSpace}${quitTxt}"
 
-  # place 'keypress' prompt directly above the image
-  # if image can be centered on screen, cursor to (2 lines above image)
-  (
-    [[ ${imgYMargin} -ge 2 ]] &&
-    cursorTo $((imgYMargin - 1)) 0
-  ) ||
-  # else, if image takes up entire screen height, cursor to (0, 0)
-  cursorTo 0 0
+  # position 'keypress' prompt 2 lines above the image
+  # if image takes up entire screen height, position at top
+  [[ ${imgYMargin} -ge 2 ]] &&
+    pcursorPos=$((imgYMargin - 1)) || 
+    pcursorPos=0
+
+  # escape whitespace, render 'keypress' prompt, break if 'q'
+  cursorTo ${pcursorPos} 0
+  formattedPrompt=$(printSpace "${promptXSpace}${promptTxt}")
   read -p "${formattedPrompt}" -rn1 key
-
-
-  ########################
-  #### get user input ####
-  ########################
-  
-  # read keypresses, break if key == 'q'
   if [[ ${key} == "q" ]]; then exit 0; fi
 done
